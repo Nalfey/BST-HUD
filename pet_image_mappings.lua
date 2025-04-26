778ff9c986abed9c22f94666e7820abae5f796f8
Nalfey <frantoms@msn.com>
2025-04-26 02:00:07 +0100

    Add files via upload

---
+++ pet_image_mappings.lua
@@ -0,0 +1,186 @@
+-- Pet Image Mappings
+-- This file maps pet names to their corresponding image files
+-- Multiple pet names can map to the same image
+
+local pet_image_mappings = {
+    -- Default image for unknown pets
+    default = 'BraveHeroGlenn.png',
+    
+    -- Mapping of pet names to image files
+    -- Format: ['pet_name'] = 'image_file.png'
+    
+    -- Beast pets
+    ['AgedAngus'] = 'AgedAngus.png',
+    ['SunburstMalfik'] = 'AgedAngus.png',
+   
+    ['AlluringHoney'] = 'AlluringHoney.png',
+    ['RedolentCandice'] = 'AlluringHoney.png',
+    
+    ['AmiableRoche'] = 'AmiableRoche.png',
+    ['TurbidToloi'] = 'AmiableRoche.png',
+   
+    ['AmigoSabotender'] = 'AmigoSabotender.png',
+    
+    ['AnklebiterJedd'] = 'AnklebiterJedd.png',
+    ['LifedrinkerLars'] = 'AnklebiterJedd.png',
+    ['MiteFamiliar'] = 'AnklebiterJedd.png',
+   
+    ['AttentiveIbuki'] = 'AttentiveIbuki.png',
+   
+    ['BlackbeardRandy'] = 'BlackbeardRandy.png',
+    ['SaberSiravarde'] = 'BlackbeardRandy.png',
+    ['TigerFamiliar'] = 'BlackbeardRandy.png',
+   
+    ['BloodclawShasra'] = 'BloodclawShasra.png',
+    
+    ['BouncingBertha'] = 'BouncingBertha.png',
+    ['ScissorlegXerin'] = 'BouncingBertha.png',
+
+    ['BrainyWaluis'] = 'BrainyWaluis.png',
+    ['DiscreetLouise'] = 'BrainyWaluis.png',
+    ['FunguarFamiliar'] = 'BrainyWaluis.png',
+
+    ['BraveHeroGlenn'] = 'BraveHeroGlenn.png',
+   
+    ['CaringKiyomaro'] = 'CaringKiyomaro.png',
+   
+    ['ChoralLeera'] = 'ChoralLeera.png',
+    ['ColibriFamiliar'] = 'ChoralLeera.png',
+   
+    ['CraftyClyvonne'] = 'CraftyClyvonne.png',
+   
+    ['CrudeRaphie'] = 'CrudeRaphie.png',
+   
+    ['CursedAnnabelle'] = 'CursedAnnabelle.png',
+    ['ChopsueyChucky'] = 'CursedAnnabelle.png',
+    ['AntlionFamiliar'] = 'CursedAnnabelle.png',
+    
+    ['DaringRoland'] = 'DaringRoland.png',
+    ['Hip.Familiar'] = 'DaringRoland.png',
+    ['FaithfulFalcor'] = 'DaringRoland.png',
+
+    ['EnergizedSefina'] = 'EnergizedSefina.png',
+    ['Y.BeetleFamiliar'] = 'EnergizedSefina.png',
+    
+    ['FatsoFargann'] = 'FatsoFargann.png',
+    
+    ['FleetReinhard'] = 'FleetReinhard.png',
+    ['SwiftSieghard'] = 'FleetReinhard.png',
+    
+    ['FlowerpotBen'] = 'FlowerpotBen.png',
+    ['FlowerpotBill'] = 'FlowerpotBen.png',
+    ['FlowerpotMerle'] = 'FlowerpotBen.png',
+
+    ['FluffyBredo'] = 'FluffyBredo.png',
+    ['AcuexFamiliar'] = 'FluffyBredo.png',
+
+
+    ['GenerousArthur'] = 'GenerousArthur.png',
+    ['GooeyGerard'] = 'GenerousArthur.png',
+
+    ['GorefangHobs'] = 'GorefangHobs.png',
+
+    ['GussyHachirobe'] = 'GussyHachirobe.png',
+    ['SpiderFamiliar'] = 'GussyHachirobe.png',
+
+    ['HeadbreakerKen'] = 'HeadbreakerKen.png',
+
+    ['HeraldHenry'] = 'HeraldHenry.png',
+    ['CourierCarrie'] = 'HeraldHenry.png',
+    ['CrabFamiliar'] = 'HeraldHenry.png',
+
+    ['HurlerPercival'] = 'HurlerPercival.png',
+    ['PanzerGalahad'] = 'HurlerPercival.png',
+    ['BeetleFamiliar'] = 'HurlerPercival.png',
+
+    ['JovialEdwin'] = 'JovialEdwin.png',
+    ['P.CrabFamiliar'] = 'JovialEdwin.png',
+
+
+    ['Left-HandedYoko'] = 'Left-HandedYoko.png',
+    ['MosquitoFamiliar'] = 'Left-HandedYoko.png',
+
+    ['LuckyLulush'] = 'LuckyLulush.png',
+
+    ['PonderingPeter'] = 'PonderingPeter.png',
+    ['DroopyDortwin'] = 'PonderingPeter.png',
+    ['KeenearedSteffi'] = 'PonderingPeter.png',
+    ['HareFamiliar'] = 'PonderingPeter.png',
+
+    ['PrestoJulio'] = 'PrestoJulio.png',
+    ['VoraciousAudrey'] = 'PrestoJulio.png',
+    ['FlytrapFamiliar'] = 'PrestoJulio.png',
+
+    ['RhymingShizuna'] = 'RhymingShizuna.png',
+    ['NurseryNazuna'] = 'RhymingShizuna.png',
+    ['LullabyMelodia'] = 'RhymingShizuna.png',
+    ['SheepFamiliar'] = 'RhymingShizuna.png',
+
+    ['SharpwitHermes'] = 'SharpwitHermes.png',
+    ['Homunculus'] = 'SharpwitHermes.png',
+
+    ['ShellbusterOrob'] = 'ShellbusterOrob.png',
+    ['MailbusterCetas'] = 'ShellbusterOrob.png',
+    ['MayflyFamiliar'] = 'ShellbusterOrob.png',
+
+    ['SlipperySilas'] = 'SlipperySilas.png',
+
+    ['StalwartAngelina'] = 'StalwartAngelina.png',
+    ['WeevilFamiliar'] = 'StalwartAngelina.png',
+
+    ['SubmergedIyo'] = 'SubmergedIyo.png',
+    ['SurgingStorm'] = 'SubmergedIyo.png',
+    ['DapperMac'] = 'SubmergedIyo.png',
+
+    ['SultryPatrice'] = 'SultryPatrice.png',
+    ['SlimeFamiliar'] = 'SultryPatrice.png',
+
+    ['SweetCaroline'] = 'SweetCaroline.png',
+
+    ['SwoopingZhivago'] = 'SwoopingZhivago.png',
+    ['AttentiveIbuki'] = 'SwoopingZhivago.png',
+
+    ['SuspiciousAlice'] = 'SuspiciousAlice.png',
+    ['BugeyedBroncha'] = 'SuspiciousAlice.png',
+    ['AmbusherAllie'] = 'SuspiciousAlice.png',
+    ['EftFamiliar'] = 'SuspiciousAlice.png',
+
+    ['ThreestarLynn'] = 'ThreestarLynn.png',
+    ['DipperYuly'] = 'ThreestarLynn.png',
+
+    ['VivaciousGaston'] = 'VivaciousGaston.png',
+    ['LynxFamiliar'] = 'VivaciousGaston.png',
+
+    ['VivaciousVickie'] = 'VivaciousVickie.png',
+    ['CaringKiyomaro'] = 'VivaciousVickie.png',
+
+    ['WarlikePatrick'] = 'WarlikePatrick.png',
+    ['AudaciousAnna'] = 'WarlikePatrick.png',
+    ['ColdbloodComet'] = 'WarlikePatrick.png',
+    ['LizardFamiliar'] = 'WarlikePatrick.png',
+}
+
+-- Function to get the image file name for a pet
+function get_pet_image(pet_name)
+    -- Clean up the pet name (remove spaces and special characters)
+    local clean_name = pet_name:gsub("%s+", ""):gsub("[^%w]", "")
+    
+    -- Try to find an exact match first
+    if pet_image_mappings[pet_name] then
+        return pet_image_mappings[pet_name]
+    end
+    
+    -- Try to find a partial match
+    for name, image in pairs(pet_image_mappings) do
+        if name:lower():find(clean_name:lower(), 1, true) then
+            return image
+        end
+    end
+    
+    -- Return default image if no match found
+    return pet_image_mappings.default
+end
+
+return {
+    get_pet_image = get_pet_image
+} 
